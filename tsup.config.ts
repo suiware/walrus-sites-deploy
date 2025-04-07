d458a14b7c8742559c53d0fb838cdf680c42d333
Konstantin Komelin <konstantin.komelin@gmail.com>
2025-04-07 05:24:57 +0200

    Switched to Commonjs because of an issue with EnvFileWriter.

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,10 +1,8 @@
 import { defineConfig } from 'tsup'
 
 export default defineConfig((options) => ({
-  entryPoints: [
-    'src/index.ts'
-  ],
-  format: ['esm'],
+  entryPoints: ['src/index.ts'],
+  format: ['cjs'],
   dts: true,
   clean: true,
   sourcemap: true,
