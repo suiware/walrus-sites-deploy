c0b9ad1f56b7dcd57b5712cdf634f40a7494c6c3
Konstantin Komelin <konstantin.komelin@gmail.com>
2025-04-07 05:56:01 +0200

    Experimenting with export options.

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'tsup'
 
 export default defineConfig((options) => ({
   entryPoints: ['src/index.ts'],
-  format: ['cjs'],
+  format: ['esm'],
   dts: true,
   clean: true,
   sourcemap: true,
